808a45c8810dc8e0090ad71f0119f687836de474
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2022-01-20 19:16:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,36 @@ def update(s):
      
 window = 10
 
-orig = "I like to do stuff with code, can you tell?"
+orig = """_________According to all known laws
+of aviation,
+
+  
+there is no way a bee
+should be able to fly.
+
+  
+Its wings are too small to get
+its fat little body off the ground.
+
+  
+The bee, of course, flies anyway
+
+  
+because bees don't care
+what humans think is impossible.
+
+  
+Yellow, black. Yellow, black.
+Yellow, black. Yellow, black.
+
+  
+Ooh, black and yellow!
+Let's shake it up a little."""
+
+orig = orig.replace("\n", " ")
+while "  " in orig:
+    orig = orig.replace("  ", " ")
+
 orig += " " * window
 orig = orig.replace(" ", "_")
 
@@ -20,7 +49,10 @@ while 1:
     s = orig[i:i+window]
     if len(s) != window:
         s += orig[:window-len(s)]
-    update(s)
+    r = update(s)
+    if "401: Unauthorized" in r.text:
+        print(r.text)
+        print("Token expired or wrong, find it in the authorization header in any discord web request")
     print(s, end="\r")
     sleep(.25)
     i += 1
